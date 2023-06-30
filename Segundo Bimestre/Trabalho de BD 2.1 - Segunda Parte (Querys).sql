37be56c034627b0249044a10a287c849493f7518
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2023-06-30 12:57:38 -0300

    Remover arquivos

--- Segundo Bimestre/Trabalho de BD 2.1 - Segunda Parte (Querys).sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE livros ADD COLUMN editora VARCHAR(100);
-
-ALTER TABLE livros DROP COLUMN ano_publicacao;
-
-UPDATE autores SET nome = "Pedro Alves" WHERE id_autor = 3;
-
-UPDATE livros SET ano_publicacao = "2023" WHERE id_categoria = 1;
-
-DELETE FROM livros WHERE id_livro = 4;
