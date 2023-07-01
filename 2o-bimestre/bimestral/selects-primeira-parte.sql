8fec90d3ae8cfdb0d4900dfb3388762ed0f0420a
Marcos Anthony <130773144+marcosanthony7@users.noreply.github.com>
2023-07-01 08:48:28 -0300

    Envio de arquivos do 2º bimestre

--- 2o-bimestre/bimestral/selects-primeira-parte.sql
+++ 2o-bimestre/bimestral/selects-primeira-parte.sql
@@ -5,20 +5,20 @@ SELECT * FROM comunidade WHERE codigo NOT IN (SELECT id_comunidade FROM particip
 # 3.
 SELECT * FROM participanteComunidade WHERE id_comunidade = 1;
 # 4.
-SELECT codigo, nome FROM grupo ORDER BY nome ASC;
-# 5.
 SELECT id, nome FROM usuario ORDER BY nome ASC;
-# 6.
+# 5.
 SELECT codigo, nome FROM comunidade ORDER BY nome DESC;
-# 7.
+# 6.
 SELECT codigo, nome FROM grupo WHERE codigo BETWEEN 1 AND 2;
-# 8.
+# 7.
 SELECT codigo, nome FROM comunidade WHERE codigo NOT BETWEEN 1 AND 2;
-# 9.
+# 8.
 SELECT id, nome FROM usuario WHERE id IN (SELECT id_usuario FROM administradorGrupo);
-# 10.
+# 9.
 SELECT COUNT(*) AS total_administradores FROM administradorGrupo;
-# 11.
+# 10.
 SELECT id, nome, email FROM usuario WHERE email LIKE '%gmail%';
-# 12.
+# 11.
 SELECT id, nome, email FROM usuario WHERE email NOT LIKE '%gmail%';
+# 12.
+SELECT id, nome FROM usuario WHERE email NOT LIKE 'a%' AND email NOT LIKE 'm%';
--- 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
+++ 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
@@ -1,65 +1,66 @@
-CREATE DATABASE Lobby;
+CREATE DATABASE lobby;
+USE lobby;
 
-CREATE TABLE Usuario(
+CREATE TABLE usuario(
 	id INT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Participante(
+CREATE TABLE participante(
 	id INT PRIMARY KEY,
     id_usuario INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
-CREATE TABLE AdministradorGrupo(
+CREATE TABLE administradorGrupo(
 	id INT PRIMARY KEY,
     id_usuario INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
+    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
 
-CREATE TABLE Comunidade(
+CREATE TABLE comunidade(
 	codigo INT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE ParticipanteComunidade(
+CREATE TABLE participanteComunidade(
 	id_participante INT NOT NULL,
     id_comunidade INT NOT NULL,
-	FOREIGN KEY (id_participante) REFERENCES Participante(id),
-    FOREIGN KEY (id_comunidade) REFERENCES Comunidade(codigo)
+	FOREIGN KEY (id_participante) REFERENCES participante(id),
+    FOREIGN KEY (id_comunidade) REFERENCES comunidade(codigo)
 );
 
-CREATE TABLE Grupo(
+CREATE TABLE grupo(
 	codigo INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     id_adminGrupo INT NOT NULL,
     id_comunidade INT NOT NULL,
-	FOREIGN KEY (id_adminGrupo) REFERENCES AdministradorGrupo(id),
-    FOREIGN KEY (id_comunidade) REFERENCES Comunidade(codigo)
+	FOREIGN KEY (id_adminGrupo) REFERENCES administradorGrupo(id),
+    FOREIGN KEY (id_comunidade) REFERENCES comunidade(codigo)
 );
 
-INSERT INTO Usuario (id, nome, email, senha)
+INSERT INTO usuario (id, nome, email, senha)
 VALUES (1, 'Marcos', 'marcos@gmail.com', 'marcos123'),
 (2, 'Anthony', 'anthony@gmail.com', 'anthony123'),
-(3, 'Thiago', 'thiago@gmail.com', 'thiago123'),
-(4, 'Alberto', 'alberto@gmail.com', 'alberto123'),
-(5, 'Lucas', 'lucas@gmail.com', 'lucas123'),
-(6, 'Maria', 'maria@gmail.com', 'maria123');
+(3, 'Thiago', 'thiago@yahoo.com', 'thiago123'),
+(4, 'Alberto', 'alberto@outlook.com', 'alberto123'),
+(5, 'Lucas', 'lucas@hotmail.com', 'lucas123'),
+(6, 'Maria', 'maria@hotmail.com', 'maria123');
 
-INSERT INTO Participante (id, id_usuario) 
+INSERT INTO participante (id, id_usuario)
 VALUES (1, 4), (2, 5), (3, 6);
 
-INSERT INTO AdministradorGrupo (id, id_usuario) 
+INSERT INTO administradorGrupo (id, id_usuario)
 VALUES (1, 1), (2, 2), (3, 3);
 
-INSERT INTO Comunidade (codigo, nome)
+INSERT INTO comunidade (codigo, nome)
 VALUES (1, 'Valorant'), (2, 'League of Legends'), (3, 'Minecraft');
 
-INSERT INTO ParticipanteComunidade (id_participante, id_comunidade) 
+INSERT INTO participanteComunidade (id_participante, id_comunidade)
 VALUES (1, 1), (2, 1), (3, 2);
 
-INSERT INTO Grupo (codigo, nome, id_adminGrupo, id_comunidade)
+INSERT INTO grupo (codigo, nome, id_adminGrupo, id_comunidade)
 VALUES (1, 'Valorant Time A', 1, 1),
 (2, 'Lolzeiros de AL', 2, 2), (3, 'Em busca da casa automatica', 3, 3);
