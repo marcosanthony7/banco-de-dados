3e3abc539ccd107d6936ae26c43f0eaf5ce017cf
Michael Miller <marcosanthony202@gmail.com>
2023-08-29 20:54:39 -0300

    Update Trabalho de BD 2.1 - Primeira Parte.sql

--- 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
+++ 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
@@ -1,47 +1,47 @@
-CREATE DATABASE lobby;
-USE lobby;
+CREATE DATABASE Lobby;
+USE Lobby;
 
-CREATE TABLE usuario(
+CREATE TABLE Usuario(
 	id INT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE participante(
+CREATE TABLE Participante(
 	id INT PRIMARY KEY,
     id_usuario INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
-CREATE TABLE administradorGrupo(
+CREATE TABLE AdministradorGrupo(
 	id INT PRIMARY KEY,
     id_usuario INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
-CREATE TABLE comunidade(
+CREATE TABLE Comunidade(
 	codigo INT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE participanteComunidade(
+CREATE TABLE ParticipanteComunidade(
 	id_participante INT NOT NULL,
     id_comunidade INT NOT NULL,
-	FOREIGN KEY (id_participante) REFERENCES participante(id),
-    FOREIGN KEY (id_comunidade) REFERENCES comunidade(codigo)
+	FOREIGN KEY (id_participante) REFERENCES Participante(id),
+    FOREIGN KEY (id_comunidade) REFERENCES Comunidade(codigo)
 );
 
-CREATE TABLE grupo(
+CREATE TABLE Grupo(
 	codigo INT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     id_adminGrupo INT NOT NULL,
     id_comunidade INT NOT NULL,
-	FOREIGN KEY (id_adminGrupo) REFERENCES administradorGrupo(id),
-    FOREIGN KEY (id_comunidade) REFERENCES comunidade(codigo)
+	FOREIGN KEY (id_adminGrupo) REFERENCES AdministradorGrupo(id),
+    FOREIGN KEY (id_comunidade) REFERENCES Comunidade(codigo)
 );
 
-INSERT INTO usuario (id, nome, email, senha)
+INSERT INTO Usuario (id, nome, email, senha)
 VALUES (1, 'Marcos', 'marcos@gmail.com', 'marcos123'),
 (2, 'Anthony', 'anthony@gmail.com', 'anthony123'),
 (3, 'Thiago', 'thiago@yahoo.com', 'thiago123'),
@@ -49,18 +49,18 @@ VALUES (1, 'Marcos', 'marcos@gmail.com', 'marcos123'),
 (5, 'Lucas', 'lucas@hotmail.com', 'lucas123'),
 (6, 'Maria', 'maria@hotmail.com', 'maria123');
 
-INSERT INTO participante (id, id_usuario)
+INSERT INTO Participante (id, id_usuario)
 VALUES (1, 4), (2, 5), (3, 6);
 
-INSERT INTO administradorGrupo (id, id_usuario)
+INSERT INTO AdministradorGrupo (id, id_usuario)
 VALUES (1, 1), (2, 2), (3, 3);
 
-INSERT INTO comunidade (codigo, nome)
+INSERT INTO Comunidade (codigo, nome)
 VALUES (1, 'Valorant'), (2, 'League of Legends'), (3, 'Minecraft');
 
-INSERT INTO participanteComunidade (id_participante, id_comunidade)
+INSERT INTO ParticipanteComunidade (id_participante, id_comunidade)
 VALUES (1, 1), (2, 1), (3, 2);
 
-INSERT INTO grupo (codigo, nome, id_adminGrupo, id_comunidade)
+INSERT INTO Grupo (codigo, nome, id_adminGrupo, id_comunidade)
 VALUES (1, 'Valorant Time A', 1, 1),
 (2, 'Lolzeiros de AL', 2, 2), (3, 'Em busca da casa automatica', 3, 3);
