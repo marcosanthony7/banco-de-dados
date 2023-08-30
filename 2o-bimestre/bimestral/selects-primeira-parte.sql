abe14c6129faab926fae20c1dd5e0b7e26704345
Marcos Anthony <marcosanthony202@gmail.com>
2023-08-29 21:03:55 -0300

    Envio de arquivo do 2º bimestre

--- 2o-bimestre/bimestral/selects-primeira-parte.sql
+++ 2o-bimestre/bimestral/selects-primeira-parte.sql
@@ -1,24 +1,24 @@
 # 1.
-SELECT id, nome FROM usuario WHERE id IN (SELECT id_usuario FROM participante);
+SELECT id, nome FROM Usuario WHERE id IN (SELECT id_usuario FROM Participante);
 # 2.
-SELECT * FROM comunidade WHERE codigo NOT IN (SELECT id_comunidade FROM participanteComunidade);
+SELECT * FROM Comunidade WHERE codigo NOT IN (SELECT id_comunidade FROM ParticipanteComunidade);
 # 3.
-SELECT * FROM participanteComunidade WHERE id_comunidade = 1;
+SELECT * FROM ParticipanteComunidade WHERE id_comunidade = 1;
 # 4.
-SELECT id, nome FROM usuario ORDER BY nome ASC;
+SELECT id, nome FROM Usuario ORDER BY nome ASC;
 # 5.
-SELECT codigo, nome FROM comunidade ORDER BY nome DESC;
+SELECT codigo, nome FROM Comunidade ORDER BY nome DESC;
 # 6.
-SELECT codigo, nome FROM grupo WHERE codigo BETWEEN 1 AND 2;
+SELECT codigo, nome FROM Grupo WHERE codigo BETWEEN 1 AND 2;
 # 7.
-SELECT codigo, nome FROM comunidade WHERE codigo NOT BETWEEN 1 AND 2;
+SELECT codigo, nome FROM Comunidade WHERE codigo NOT BETWEEN 1 AND 2;
 # 8.
-SELECT id, nome FROM usuario WHERE id IN (SELECT id_usuario FROM administradorGrupo);
+SELECT id, nome FROM Usuario WHERE id IN (SELECT id_usuario FROM AdministradorGrupo);
 # 9.
-SELECT COUNT(*) AS total_administradores FROM administradorGrupo;
+SELECT COUNT(*) AS total_administradores FROM AdministradorGrupo;
 # 10.
-SELECT id, nome, email FROM usuario WHERE email LIKE '%gmail%';
+SELECT id, nome, email FROM Usuario WHERE email LIKE '%gmail%';
 # 11.
-SELECT id, nome, email FROM usuario WHERE email NOT LIKE '%gmail%';
+SELECT id, nome, email FROM Usuario WHERE email NOT LIKE '%gmail%';
 # 12.
-SELECT id, nome FROM usuario WHERE email NOT LIKE 'a%' AND email NOT LIKE 'm%';
+SELECT id, nome FROM Usuario WHERE email NOT LIKE 'a%' AND email NOT LIKE 'm%';
