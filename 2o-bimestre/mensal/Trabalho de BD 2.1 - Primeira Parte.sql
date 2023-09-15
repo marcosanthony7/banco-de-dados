397692cb8f7c9f90c6172a47f32f844893264142
Marcos Anthony <marcosanthony202@gmail.com>
2023-09-14 21:09:59 -0300

    Atualização de arquivo do 2º bimestre

--- 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
+++ 2o-bimestre/mensal/Trabalho de BD 2.1 - Primeira Parte.sql
@@ -2,27 +2,37 @@ CREATE DATABASE Lobby;
 USE Lobby;
 
 CREATE TABLE Usuario(
-	id INT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    apelido VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    senha VARCHAR(50) NOT NULL,
+    foto VARCHAR(100),
+    banner VARCHAR(100),
+    descricao VARCHAR(200),
+    titulo VARCHAR(50),
+	avaliacao INT,
+    seguidores INT,
+    seguindo INT
 );
 
 CREATE TABLE Participante(
-	id INT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 CREATE TABLE AdministradorGrupo(
-	id INT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
     id_usuario INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 CREATE TABLE Comunidade(
-	codigo INT PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL
+	codigo INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    foto VARCHAR(100),
+	descricao VARCHAR(200)
 );
 
 CREATE TABLE ParticipanteComunidade(
@@ -33,12 +43,41 @@ CREATE TABLE ParticipanteComunidade(
 );
 
 CREATE TABLE Grupo(
-	codigo INT NOT NULL,
-    nome VARCHAR(100) NOT NULL,
+	codigo INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    foto VARCHAR(100),
+	descricao VARCHAR(200),
     id_adminGrupo INT NOT NULL,
     id_comunidade INT NOT NULL,
 	FOREIGN KEY (id_adminGrupo) REFERENCES AdministradorGrupo(id),
     FOREIGN KEY (id_comunidade) REFERENCES Comunidade(codigo)
+);
+
+CREATE TABLE Amigos(
+	FK_id_usuario1 INT NOT NULL,
+    FK_id_usuario2 INT NOT NULL,
+    FOREIGN KEY(FK_id_usuario1) REFERENCES Usuario(id),
+    FOREIGN KEY(FK_id_usuario2) REFERENCES Usuario(id),
+    PRIMARY KEY(FK_id_usuario1, FK_id_usuario2)
+);
+
+CREATE TABLE Mensagens(
+	FK_id_origem INT NOT NULL,
+    FK_id_destino INT NOT NULL,
+    mensagem VARCHAR(100),
+    FOREIGN KEY(FK_id_origem) REFERENCES Usuario(id),
+    FOREIGN KEY(FK_id_destino) REFERENCES Usuario(id),
+    PRIMARY KEY(FK_id_origem, FK_id_destino)
+);
+
+CREATE TABLE Postagens(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario INT NOT NULL,
+    foto VARCHAR(100),
+    link_video VARCHAR(100),
+    descricao VARCHAR(200),
+    likes INT,
+    FOREIGN KEY (id_usuario) REFERENCES Usuario(id)
 );
 
 INSERT INTO Usuario (id, nome, email, senha)
